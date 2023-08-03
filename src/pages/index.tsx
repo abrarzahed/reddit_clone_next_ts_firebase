1befe15938be286936014aef9cbf3324bdf97297
abrarzahed <abrarzahed6986@gmail.com>
2023-08-03 21:13:48 +0600

    fixed errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body>
+      <main>
         <Box
           bg={`layer2`}
           border={`1px solid`}
